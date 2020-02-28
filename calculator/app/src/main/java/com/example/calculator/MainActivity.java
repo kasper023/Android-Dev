b6dcae0c67f83e737273ee0eced02324cb93502e
kasper023 <askar.baygenzhin@gmail.com>
2020-02-29 00:16:30 +0600

    calculator-final

--- calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -25,35 +25,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        input=(TextView) findViewById(R.id.input);
+        input = (TextView) findViewById(R.id.input);
         signBox = (TextView) findViewById(R.id.sign);
         values = (TextView) findViewById((R.id.values));
-        values.setText(value1 + "___ " + value2 + "");
         hasdot = false;
         signBool = false;
         zeroBool = false;
 
         if(savedInstanceState != null)
         {
+            input.setText(savedInstanceState.getString("input"));
             sign = savedInstanceState.getString("sign");
             value1 = savedInstanceState.getString("value1");
             value2 = savedInstanceState.getString("value2");
             value3 = savedInstanceState.getString("value3");
             num1 = savedInstanceState.getDouble("num1");
             num2 = savedInstanceState.getDouble("num2");
+
             result = savedInstanceState.getDouble("result");
             hasdot = savedInstanceState.getBoolean("hasdot");
             signBool = savedInstanceState.getBoolean("signBool");
             zeroBool = savedInstanceState.getBoolean("zeroBool");
             signBox.setText(sign);
-            if(sign != null)
-            {
-                input.setText(value2);
-            }
-            else
-            {
-                input.setText(value1);
-            }
+//            if(sign != null)
+//            {
+//                input.setText(value2);
+//            }
+//            else
+//            {
+//                input.setText(value1);
+//            }
+            values.setText(value1 + "_" + value2 + "_" + value3);
         }
 
     }
@@ -66,7 +68,8 @@ public class MainActivity extends AppCompatActivity {
         {
             outState.putString("sign",sign);
         }
-
+        if(input.getText() != "" && input.getText() != null)
+            outState.putString("input", input.getText().toString());
         if(value1 == "" || value1 == null) {
             if(input.getText() == "" || input.getText() == null)
                 outState.putString("value1", "");
@@ -430,228 +433,227 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void btnClick_sin(View view)
+    {
+        if(input.getText().equals(""))
+        {
+            signBox.setText("Error!");
+        }
+        else
+        {
+            if(signBox.getText().equals(("Error!")))
+            {
+                signBox.setText("");
+            }
+            String value3 = input.getText().toString();
+            double num3 = Double.parseDouble(value3);
+            result = Math.sin(num3);
+            input.setText(result + "");
+            result = null;
+        }
+    }
 
-//    public void btnClick_sin(View view)
-//    {
-//        if(input.getText().equals(""))
-//        {
-//            signBox.setText("Error!");
-//        }
-//        else
-//        {
-//            if(signBox.getText().equals(("Error!")))
-//            {
-//                signBox.setText("");
-//            }
-//            String value3 = input.getText().toString();
-//            double num3 = Double.parseDouble(value3);
-//            result = Math.sin(num3);
-//            input.setText(result + "");
-//            result = null;
-//        }
-//    }
-//
-//    public void btnClick_cos(View view)
-//    {
-//        if(input.getText().equals(""))
-//        {
-//            signBox.setText("Error!");
-//        }
-//        else
-//        {
-//            if(signBox.getText().equals(("Error!")))
-//            {
-//                signBox.setText("");
-//            }
-//            String value3 = input.getText().toString();
-//            double num3 = Double.parseDouble(value3);
-//            result = Math.cos(num3);
-//            input.setText(result + "");
-//            result = null;
-//        }
-//    }
-//
-//    public void btnClick_tan(View view)
-//    {
-//        if(input.getText().equals(""))
-//        {
-//            signBox.setText("Error!");
-//        }
-//        else
-//        {
-//            if(signBox.getText().equals(("Error!")))
-//            {
-//                signBox.setText("");
-//            }
-//            String value3 = input.getText().toString();
-//            double num3 = Double.parseDouble(value3);
-//            result = Math.tan(num3);
-//            input.setText(result + "");
-//            result = null;
-//        }
-//    }
-//
-//    public void btnClick_percent(View view)
-//    {
-//        if(input.getText().equals(""))
-//        {
-//            signBox.setText("Error!");
-//        }
-//        else
-//        {
-//            if(signBox.getText().equals(("Error!")))
-//            {
-//                signBox.setText("");
-//            }
-//            String value3 = input.getText().toString();
-//            double num3 = Double.parseDouble(value3);
-//            result = num3 * 0.01;
-//            input.setText(result + "");
-//            result = null;
-//        }
-//    }
-//
-//    public void btnClick_ln(View view)
-//    {
-//        if(input.getText().equals(""))
-//        {
-//            signBox.setText("Error!");
-//        }
-//        else
-//        {
-//            if(signBox.getText().equals(("Error!")))
-//            {
-//                signBox.setText("");
-//            }
-//            String value3 = input.getText().toString();
-//            double num3 = Double.parseDouble(value3);
-//            if(num3 <= 0)
-//            {
-//                signBox.setText("Error!");
-//            }
-//            else
-//            {
-//                result = Math.log(num3);
-//                input.setText(result + "");
-//                result = null;
-//            }
-//        }
-//    }
-//
-//    public void btnClick_log(View view)
-//    {
-//        if(input.getText().equals(""))
-//        {
-//            signBox.setText("Error!");
-//        }
-//        else
-//        {
-//            if(signBox.getText().equals(("Error!")))
-//            {
-//                signBox.setText("");
-//            }
-//            String value3 = input.getText().toString();
-//            double num3 = Double.parseDouble(value3);
-//            if(num3 <= 0)
-//            {
-//                signBox.setText("Error!");
-//            }
-//            else
-//            {
-//                result = Math.log10(num3);
-//                input.setText(result + "");
-//                result = null;
-//            }
-//        }
-//    }
-//
-//    public void btnClick_sqr_n(View view)
-//    {
-//        if(signBool == false)
-//        {
-//            if(input.getText().equals(""))
-//            {
-//                value1 = "0";
-//                sign = "^m";
-//                input.setText(null);
-//                signBox.setText("^m");
-//                hasdot = false;
-//                signBool = true;
-//            }
-//            else {
-//                sign = "^m";
-//                value1 = input.getText().toString();
-//                input.setText(null);
-//                signBox.setText("^m");
-//                hasdot = false;
-//                value3 = value1;
-//            }
-//        }
-//        else
-//        {
-//            input.setText("");
-//            value1 = value3;
-//            sign = "^m";
-//            signBox.setText("^m");
-//        }
-//    }
-//
-//
-//    public void btnClick_sqrt_n(View view)
-//    {
-//        if(signBool == false)
-//        {
-//            if(input.getText().equals(""))
-//            {
-//                value1 = "0";
-//                sign = "n√";
-//                input.setText(null);
-//                signBox.setText("n√");
-//                hasdot = false;
-//                signBool = true;
-//            }
-//            else {
-//                sign = "n√";
-//                value1 = input.getText().toString();
-//                input.setText(null);
-//                signBox.setText("n√");
-//                hasdot = false;
-//                value3 = value1;
-//            }
-//        }
-//        else
-//        {
-//            input.setText("");
-//            value1 = value3;
-//            sign = "n√";
-//            signBox.setText("n√");
-//        }
-//    }
-//
-//    public void btnClick_factorial(View view)
-//    {
-//        if(input.getText().equals(""))
-//        {
-//            signBox.setText("Error!");
-//        }
-//        else
-//        {
-//            if(signBox.getText().equals(("Error!")))
-//            {
-//                signBox.setText("");
-//            }
-//            String value3 = input.getText().toString();
-//            int num3 = Integer.parseInt(value3);
-//            int c = 1;
-//            for (int i = 2; i <= num3; i++)
-//            {
-//                c = c * i;
-//            }
-//            result = Double.valueOf(c);
-//            input.setText(result + "");
-//            result = null;
-//        }
-//    }
+    public void btnClick_cos(View view)
+    {
+        if(input.getText().equals(""))
+        {
+            signBox.setText("Error!");
+        }
+        else
+        {
+            if(signBox.getText().equals(("Error!")))
+            {
+                signBox.setText("");
+            }
+            String value3 = input.getText().toString();
+            double num3 = Double.parseDouble(value3);
+            result = Math.cos(num3);
+            input.setText(result + "");
+            result = null;
+        }
+    }
+
+    public void btnClick_tan(View view)
+    {
+        if(input.getText().equals(""))
+        {
+            signBox.setText("Error!");
+        }
+        else
+        {
+            if(signBox.getText().equals(("Error!")))
+            {
+                signBox.setText("");
+            }
+            String value3 = input.getText().toString();
+            double num3 = Double.parseDouble(value3);
+            result = Math.tan(num3);
+            input.setText(result + "");
+            result = null;
+        }
+    }
+
+    public void btnClick_percent(View view)
+    {
+        if(input.getText().equals(""))
+        {
+            signBox.setText("Error!");
+        }
+        else
+        {
+            if(signBox.getText().equals(("Error!")))
+            {
+                signBox.setText("");
+            }
+            String value3 = input.getText().toString();
+            double num3 = Double.parseDouble(value3);
+            result = num3 * 0.01;
+            input.setText(result + "");
+            result = null;
+        }
+    }
+
+    public void btnClick_ln(View view)
+    {
+        if(input.getText().equals(""))
+        {
+            signBox.setText("Error!");
+        }
+        else
+        {
+            if(signBox.getText().equals(("Error!")))
+            {
+                signBox.setText("");
+            }
+            String value3 = input.getText().toString();
+            double num3 = Double.parseDouble(value3);
+            if(num3 <= 0)
+            {
+                signBox.setText("Error!");
+            }
+            else
+            {
+                result = Math.log(num3);
+                input.setText(result + "");
+                result = null;
+            }
+        }
+    }
+
+    public void btnClick_log(View view)
+    {
+        if(input.getText().equals(""))
+        {
+            signBox.setText("Error!");
+        }
+        else
+        {
+            if(signBox.getText().equals(("Error!")))
+            {
+                signBox.setText("");
+            }
+            String value3 = input.getText().toString();
+            double num3 = Double.parseDouble(value3);
+            if(num3 <= 0)
+            {
+                signBox.setText("Error!");
+            }
+            else
+            {
+                result = Math.log10(num3);
+                input.setText(result + "");
+                result = null;
+            }
+        }
+    }
+
+    public void btnClick_sqr_n(View view)
+    {
+        if(signBool == false)
+        {
+            if(input.getText().equals(""))
+            {
+                value1 = "0";
+                sign = "^m";
+                input.setText(null);
+                signBox.setText("^m");
+                hasdot = false;
+                signBool = true;
+            }
+            else {
+                sign = "^m";
+                value1 = input.getText().toString();
+                input.setText(null);
+                signBox.setText("^m");
+                hasdot = false;
+                value3 = value1;
+            }
+        }
+        else
+        {
+            input.setText("");
+            value1 = value3;
+            sign = "^m";
+            signBox.setText("^m");
+        }
+    }
+
+
+    public void btnClick_sqrt_n(View view)
+    {
+        if(signBool == false)
+        {
+            if(input.getText().equals(""))
+            {
+                value1 = "0";
+                sign = "nv";
+                input.setText(null);
+                signBox.setText("nv");
+                hasdot = false;
+                signBool = true;
+            }
+            else {
+                sign = "nv";
+                value1 = input.getText().toString();
+                input.setText(null);
+                signBox.setText("nv");
+                hasdot = false;
+                value3 = value1;
+            }
+        }
+        else
+        {
+            input.setText("");
+            value1 = value3;
+            sign = "nv";
+            signBox.setText("nv");
+        }
+    }
+
+    public void btnClick_factorial(View view)
+    {
+        if(input.getText().equals(""))
+        {
+            signBox.setText("Error!");
+        }
+        else
+        {
+            if(signBox.getText().equals(("Error!")))
+            {
+                signBox.setText("");
+            }
+            String value3 = input.getText().toString();
+            int num3 = Integer.parseInt(value3);
+            double c = 1;
+            for (int i = 2; i <= num3; i++)
+            {
+                c = c * i;
+            }
+            result = Double.valueOf(c);
+            input.setText(result + "");
+            result = null;
+        }
+    }
 
 
 
@@ -672,79 +674,6 @@ public class MainActivity extends AppCompatActivity {
             {
                 default:
                     break;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                 case "+":
                     result = num1 + num2;
                     input.setText(result + "");
@@ -784,27 +713,31 @@ public class MainActivity extends AppCompatActivity {
                         signBool = false;
                         break;
                     }
-//                case "^m":
-//                    result = Math.pow(num1,num2);
-//                    input.setText(result + "");
-//                    sign = null;
-//                    signBox.setText(null);
-//                    hasdot = true;
-//                    signBool = false;
-//                    break;
-//                case "n√":
-//                    num2 = 1.0 / num2;
-//                    result = Math.pow(num1,num2);
-//                    String fresult = String.format("%.2f",result);
-//                    input.setText(fresult);
-//                    sign = null;
-//                    signBox.setText(null);
-//                    hasdot = true;
-//                    signBool = false;
-//                    break;
+                case "^m":
+                    result = Math.pow(num1,num2);
+                    input.setText(result + "");
+                    sign = null;
+                    signBox.setText(null);
+                    hasdot = true;
+                    signBool = false;
+                    break;
+                case "nv":
+                    num2 = 1.0 / num2;
+                    result = Math.pow(num1,num2);
+                    String fresult = String.format("%.2f",result);
+                    input.setText(fresult);
+                    sign = null;
+                    signBox.setText(null);
+                    hasdot = true;
+                    signBool = false;
+                    break;
             }
         }
-        else input.setText("Error!");
+        else
+        {
+            input.setHint("0");
+            signBox.setText("Error!");
+        }
 
         values.setText(value1 + "___ " + value2 + "");
     }
@@ -825,19 +758,18 @@ public class MainActivity extends AppCompatActivity {
         {
             input.setHint("0");
         }
+        else if(input.getText().equals("Infinity") || input.getText().equals("Error!"))
+        {
+            input.setText("");
+            input.setHint("0");
+        }
         else
         {
             int len = input.getText().length();
             String s = input.getText().toString();
             if(s.charAt((len-1)) == '.')
-            {
                 hasdot = false;
-                input.setText(input.getText().subSequence(0, input.getText().length() - 1));
-            }
-            else
-            {
-                input.setText(input.getText().subSequence(0, input.getText().length() - 1));
-            }
+            input.setText(input.getText().subSequence(0, input.getText().length() - 1));
         }
     }
 
